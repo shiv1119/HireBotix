87a6068cd7c45fc44289f36cbcb399cb8b319084
shiv1119 <shivnandanverma63@gmail.com>
2026-03-06 13:09:52 +0530

    Add roles to user table

--- app/models/users.py
+++ app/models/users.py
@@ -1,13 +1,21 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from enum import Enum
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, Enum as SQLEnum
 from sqlalchemy.sql import func
 from app.db.database import Base
 
+class RoleEnum(str, Enum):
+    HR = "HR"
+    USER = "USER"
+    ADMIN = "ADMIN"
+
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=True)
     linkedin_id = Column(String, unique=True, nullable=True)
+    role = Column(String, default=RoleEnum.USER.value, nullable=False)
     is_active = Column(Boolean, default=True)
     is_deleted = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
